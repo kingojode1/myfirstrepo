a0c7acec1328fe903ce5f2e1e16fce693bde3faf
kingojode1 <kingakor2442@gmail.com>
2021-11-01 14:54:44 +0100

    my new changes

--- main.js
+++ main.js
@@ -6,3 +6,52 @@ navBar.addEventListener("click", function(){
     navBar.classList.toggle("active");
     navBarMenu.classList.toggle("is-active");
 })
+
+
+
+let persons = [{
+    photo:"imageone1.jpg"
+   
+    
+},
+{
+    photo:"imagetwo2.jpg"
+   
+   
+},
+{
+    photo:"imagethree3.jpg"
+  
+   
+},
+{
+    photo:"imagefour4.jpg"
+   
+}
+];
+
+
+let image = document.querySelector('.our-image');
+
+
+
+let currentValue = 0;
+
+window.addEventListener('DOMContentLoaded',function(){
+    showPerson();
+});
+
+setInterval(function(){
+    currentValue++;
+    if(currentValue === persons.length){
+        currentValue = 0;
+    }
+    showPerson();
+},4000);
+
+function showPerson(){
+    let item = persons[currentValue];
+    image.src = item.photo;
+ 
+  
+}
